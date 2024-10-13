84a81733db7eae1e2f08c6d70de11bedd1df72cf
Ann Melnikova <amelniko@made-f0Br6s19.clusters.42paris.fr>
2024-10-13 12:48:25 +0200

    Adding logic to copy return value from getenv to an allocated string to be able to free it later

--- srcs/parsing/handle_dollar.c
+++ srcs/parsing/handle_dollar.c
@@ -18,9 +18,10 @@ static char *add_env_var(t_all *all, t_cmd *cmd)
     char    *env_val;
     char    *env_name;
     int     len_name;
+    char    *ret;
 
     len_name = 0;
-    while (is_white_space(all->line[len_name]))
+    while (is_white_space(all->line[len_name]) && all->line[len_name])
         len_name++;
     env_name = (char *) malloc((len_name + 1) * sizeof(char));
     if (!env_name)
@@ -28,6 +29,10 @@ static char *add_env_var(t_all *all, t_cmd *cmd)
     ft_strlcpy(env_name, all->line, len_name + 1);
     env_val = getenv(env_name);
     free(env_name);
+    ret = (char *) malloc(sizeof(char) * (ft_strlen(env_val) + 1));
+    if (!ret)
+        error("hadnle_dollar: Malloc error\n", all);
+    ft_strlcpy(ret, env_val, ft_strlen(env_val) + 1);
     all->line += len_name;
-    return (env_val);
+    return (ret);
 }
