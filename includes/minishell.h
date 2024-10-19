24c301d28844896844cd31b468976218d2aa6001
Ann Melnikova <amelniko@made-f0Ar4s3.clusters.42paris.fr>
2024-10-19 15:04:59 +0200

    Changing of return type for input and output redirections handlers

--- includes/minishell.h
+++ includes/minishell.h
@@ -47,8 +47,8 @@ int is_redir(char car);
 char    *handle_dollar(t_all *all, t_cmd *cmd, char car);
 char    *handle_redir(t_all *all, t_cmd *cmd);
 char *read_addr(t_all *all, t_cmd *cmd);
-char    *handle_input(t_all *all, t_cmd *cmd);
-char    *handle_output(t_all *all, t_cmd *cmd);
+void    handle_output(t_all *all, t_cmd *cmd);
+void    handle_input(t_all *all, t_cmd *cmd);
 char    *handle_quotes(t_all *all, t_cmd *cmd);
 char    *handle_word(t_all *all, t_cmd *cmd, int in_dollar);
 //finalization part
