ff455b906bb2026aa9376a0ab978ccbd852e59fe
Talan <talanlipanda@gmail.com>
2024-11-03 06:31:12 +0100

    cd with '-' or '~' currently working --> need to verify leaks with valgrind

--- srcs/builtins/cd.c
+++ srcs/builtins/cd.c
@@ -11,23 +11,45 @@
 /* ************************************************************************** */
 #include "../../includes/minishell.h"
 
+static	int	expand_wave(t_msh *msh, char *str)
+{
+	char	*path;
+
+	if (str && str[0] == '~')
+		str++;
+	path = tjoin(tstrdup(env_retrieve_var(msh->env, "HOME")->var), str);
+	if (!chdir(path))
+	{
+		free(path);
+		return (1);
+	}
+	free(path);
+	return (0);
+}
+
 int	cd(t_msh *msh, t_args *argv)
 {
 	t_args	export;
+	int		pass;
 
+	pass = 0;
 	if (argv && argv->next)
 		printf("cd: too many arguments\n");
 	else if (!argv && !env_retrieve_var(msh->env, "HOME"))
 		printf("cd: HOME not set\n");
-	// else if (argv && !tstrcmp(argv->arg, "-"))
-		// if (env_retrieve_var(msh->env, "OLDPWD") && (chdir(env_retrieve_var(msh->env, "OLDPWD")->var)))
-			// export
-	else if ((!argv && chdir(env_retrieve_var(msh->env, "HOME")->var) == 0)
-			|| (argv->arg && chdir(argv->arg) == 0))
+	if (argv && argv->arg && argv->arg[0] == '~' && expand_wave(msh, argv->arg))
+		pass = 22;
+	else if ((argv && !tstrcmp(argv->arg, "-")) && (env_retrieve_var(msh->env, "OLDPWD") && (!chdir(env_retrieve_var(msh->env, "OLDPWD")->var))))
+		pass = 22;
+	else if (!argv && env_retrieve_var(msh->env, "HOME") && (!chdir(env_retrieve_var(msh->env, "HOME")->var)))
+		pass = 22;
+	else if (argv->arg && chdir(argv->arg) == 0)
+		pass = 22;
+	if (pass == 22)
 	{
 		export.arg = tjoin(tjoin(tstrdup("OLDPWD"), "="), msh->pwd);
 		if (!env_retrieve_var(msh->env, "OLDPWD"))
-			export_def(msh, &export.arg = );
+			export_def(msh, &export);
 		if (env_retrieve_var(msh->env, "OLDPWD"))
 		{
 			free(env_retrieve_var(msh->env, "OLDPWD")->var);
