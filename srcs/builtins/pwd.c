d42b342eb8e70d1578eb56e6f9b9545a0508f03e
talanespdj <talanlipanda@gmail.com>
2024-11-05 18:24:18 +0100

    pwd msg working // adding test.sh fdmonitor.sh

--- srcs/builtins/pwd.c
+++ srcs/builtins/pwd.c
@@ -6,7 +6,7 @@
 /*   By: tespandj <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 19:54:40 by tespandj          #+#    #+#             */
-/*   Updated: 2024/10/14 19:54:41 by tespandj         ###   ########.fr       */
+/*   Updated: 2024/11/05 17:49:41 by tespandj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../../includes/minishell.h"
@@ -16,7 +16,7 @@ int	pwd(t_msh *msh)
 	if (msh->pwd)
 		printf("%s\n", msh->pwd);
 	else
-		printf("error retrieving current directory: getcwd: \
-			cannot access parent directories: No such file or directory\n");
+		printf("error retrieving current directory: getcwd:\
+cannot access parent directories: No such file or directory\n");
 	return (1);
 }
